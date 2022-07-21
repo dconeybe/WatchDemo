c6ffab6a84f39a62c92ba1fb25a5d64411409e1f
Denver Coneybeare <dconeybe@google.com>
2022-07-21 11:37:25 -0400

    main.py: create Command base class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections.abc import Sequence
 
+import abc
 import grpc
 import re
 
@@ -15,47 +16,65 @@ PARENT = "projects/dconeybe-testing/databases/(default)/documents"
 COLLECTION_ID = "WatchDemo"
 
 
-def populate(stub: firestore_pb2_grpc.FirestoreStub) -> None:
-  for i in range(5):
-    request = firestore_pb2.CreateDocumentRequest(
+class Command(abc.ABC):
+
+  @abc.abstractmethod
+  def run(self, stub: firestore_pb2_grpc.FirestoreStub) -> None:
+    raise NotImplementedError
+
+
+class InitializeCommand(Command):
+
+  def run(self, stub: firestore_pb2_grpc.FirestoreStub) -> None:
+    for i in range(5):
+      request = firestore_pb2.CreateDocumentRequest(
+        parent=PARENT,
+        collection_id=COLLECTION_ID,
+        document_id=f"Doc{i}",
+        document=document_pb2.Document(
+          fields={
+            "key": document_pb2.Value(
+              integer_value=42,
+            ),
+          },
+        ),
+      )
+
+      logging.info("Creating document: %s", request.document_id)
+      stub.CreateDocument(request)
+
+
+class ListDocumentsCommand(Command):
+
+  def run(self, stub: firestore_pb2_grpc.FirestoreStub) -> None:
+    request = firestore_pb2.ListDocumentsRequest(
       parent=PARENT,
       collection_id=COLLECTION_ID,
-      document_id=f"Doc{i}",
-      document=document_pb2.Document(
-        fields={
-          "key": document_pb2.Value(
-            integer_value=42,
-          ),
-        },
-      ),
     )
 
-    logging.info("Creating document: %s", request.document_id)
-    stub.CreateDocument(request)
+    logging.info("Listing documents...")
+    response = stub.ListDocuments(request)
+
+    count = 0
+    for document in response.documents:
+      count += 1
+      logging.info(
+        "#%s: %s key=%s",
+        count,
+        document.name,
+        str(document.fields.get("key")).strip()
+      )
+
+    logging.info("Found %s documents", count)
 
 
-def list_documents(stub: firestore_pb2_grpc.FirestoreStub) -> None:
-  request = firestore_pb2.ListDocumentsRequest(
-    parent=PARENT,
-    collection_id=COLLECTION_ID,
-  )
-
-  logging.info("Listing documents...")
-  response = stub.ListDocuments(request)
-  count = 0
-  for document in response.documents:
-    count += 1
-    logging.info("#%s: %s key=%s", count, document.name, str(document.fields.get("key")).strip())
-  logging.info("Found %s documents", count)
-
-
-class UpdateDocument:
+class UpdateDocumentCommand(Command):
 
   def __init__(self, document_id: str, value: int) -> None:
     self.document_id = document_id
     self.value = value
 
-  def __call__(self, stub: firestore_pb2_grpc.FirestoreStub) -> None:
+  def run(self, stub: firestore_pb2_grpc.FirestoreStub) -> None:
     request = firestore_pb2.UpdateDocumentRequest(
       document=document_pb2.Document(
         name=f"{PARENT}/{COLLECTION_ID}/{self.document_id}",
@@ -79,27 +98,28 @@ class UpdateDocument:
 
 def main(argv: Sequence[str]) -> None:
   if len(argv) == 1:
-    raise app.UsageError("no command specified; supported commands are: populate ls")
+    raise app.UsageError("no command specified; supported commands are: init ls set:DocN=999")
   elif len(argv) == 2:
     command_str = argv[1]
   elif len(argv) > 2:
     raise app.UsageError(f"unexpected argument: {argv[2]}")
 
-  if command_str == "populate":
-    command = populate
+  command: Command
+  if command_str == "init":
+    command = InitializeCommand()
   elif command_str == "ls":
-    command = list_documents
+    command = ListDocumentsCommand()
   elif command_str.startswith("set:"):
     match = re.fullmatch(r"set:(\w+)=(\d+)", command_str)
     if not match:
       raise app.UsageError(f"invalid set command: {command_str} (expected set:DocId=IntValue)")
-    command = UpdateDocument(document_id=match.group(1), value=int(match.group(2)))
+    command = UpdateDocumentCommand(document_id=match.group(1), value=int(match.group(2)))
   else:
     raise app.UsageError(f"unsupported command: {command_str}")
 
   with grpc.insecure_channel('localhost:8080') as grpc_channel:
     stub = firestore_pb2_grpc.FirestoreStub(grpc_channel)
-    command(stub)
+    command.run(stub)
 
 
 if __name__ == "__main__":
